bcdef1a2d6d739535a521a3e8d79f950508edc8f
Abhinavsinghnegi <abhinavnegi842@gmail.com>
2022-12-18 00:24:47 +0530

    feat:render navbar

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { environment } from './../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,
