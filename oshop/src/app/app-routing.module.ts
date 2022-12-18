452082ddf0ff84018c58ad69dbd4d9de5855953a
Abhinavsinghnegi <abhinavnegi842@gmail.com>
2022-12-18 13:45:28 +0530

    fix:clean the navbar and add route for my-orders

--- oshop/src/app/app-routing.module.ts
+++ oshop/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path:'products', component:ProductsComponent},
   {path:'shopping-cart', component:ShoppingCartComponent},
   {path:'check-out', component:CheckOutComponent},
+  {path:'my-orders', component:MyOrdersComponent},
   {path:'order-success', component:OrderSuccessComponent},
   {path:'login', component:LoginComponent},
   {path:'admin/products', component:AdminProductsComponent},
