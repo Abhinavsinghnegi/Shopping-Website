4b200b2703e4c2f720f2634281d05b9994f8e266
Abhinav <abhinavnegi842@gmail.com>
2023-01-04 23:49:48 +0530

    feat : implement logout

--- oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'bs-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth : AngularFireAuth) {
+    this.afAuth.authState.subscribe((x) => console.log(x));
+   }
 
   ngOnInit(): void {
+  }
+
+  logout = () => {
+    this.afAuth.signOut();
   }
 
 }
